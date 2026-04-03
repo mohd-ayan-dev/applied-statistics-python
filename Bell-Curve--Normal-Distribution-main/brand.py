be2cb955030e9b6b29cf829a0466ee9a0f687b84
Mohammed Ayan <66216467+mohd-ayan-dev@users.noreply.github.com>
2026-04-03 05:54:58 +0530

    Delete Bell-Curve--Normal-Distribution-main directory

--- Bell-Curve--Normal-Distribution-main/brand.py
+++
@@ -1,7 +0,0 @@
-import plotly.figure_factory as ff
-import pandas as pd
-import csv
-
-df = pd.read_csv("data.csv")
-fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"], show_hist=True)
-fig.show()
